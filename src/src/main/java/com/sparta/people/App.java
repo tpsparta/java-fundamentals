a5db8a76175d4ddc76eb0be76657ce9ff302dc74
tpsparta <tprior@spartaglobal.com>
2026-08-28 10:31:51 +0000

    Updated equals method for objects

--- src/src/main/java/com/sparta/people/App.java
+++ src/src/main/java/com/sparta/people/App.java
@@ -4,5 +4,23 @@ public class App {
 
     public  static void main(String[] args) {
 
+        Customer c1 = new Customer("Nish", "Mandal");
+        Customer c2 = new Customer("Nish", "Mandal");
+        Customer c3 = new Customer("Nish", "Mandal");
+        
+        
+        // testing the equals() contract:
+        
+        // reflexive - should return true
+        System.out.println(c1.equals(c1));
+        
+        // symmetric - both should return true
+        System.out.println(c1.equals(c2));
+        System.out.println(c2.equals(c1));
+        
+        // transitive - both should return true
+        System.out.println(c2.equals(c3));
+        System.out.println(c1.equals(c3));
+	
     }
 }
